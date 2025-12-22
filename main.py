e717321192b821521703f71e0532bf92e0816a61
MichalKrywult <krywult.mich@gmail.com>
2025-12-22 22:38:11 +0100

    Create new functions: awatar deleted info and edited create_embed

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from discord.ext import commands
 from dotenv import load_dotenv
 
 
-async def create_embed(ctx, title, description, color, footer):
+def create_embed(title, description, color, footer):
     embed = discord.Embed(
         title=title,
         description=description,
@@ -15,50 +15,62 @@ async def create_embed(ctx, title, description, color, footer):
     embed.set_footer(text=footer)
     return embed
 
+
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
 intents = discord.Intents.default()
-intents.message_content = True  # Bez tego bot nie zobaczy "!komenda"
+intents.message_content = True
 bot = commands.Bot(command_prefix='!', intents=intents)
+
 
 @bot.event
 async def on_ready():
     print(f'Zalogowano jako {bot.user}')
+
+
 @bot.event
 async def on_command_error(ctx, error):
     if isinstance(error, commands.CommandNotFound):
         await ctx.send("Nieznana komenda")
 
+
 @bot.command()
 async def czesc(ctx):
-    await ctx.send(f'Hej {ctx.author.name}! ')
-@bot.command()
+    await ctx.send(f'Hej {ctx.author.name}!')
+
+
 @bot.command()
 async def kostka(ctx):
     wynik = random.randint(1, 6)
-    if wynik%2==0:
+
+    if wynik % 2 == 0:
         wybrany_kolor = discord.Color.green()
     else:
-           discord.Color.red()
+        wybrany_kolor = discord.Color.red()
+
     embed = create_embed(
-        "Rzut kostką 🎲", 
-        f"Wyrzuciłeś: **{wynik}**", 
-        wybrany_kolor, 
-        f"Wywołane przez {ctx.author.name}",
-        ""
-    )    
-    await ctx.send(embed=embed)
-@bot.command()
-async def info(ctx):
-    embed= create_embed(
-        ctx, 
-        "Informacje o bocie", 
-        "Bot działa", 
-        discord.Color.green(), 
-        "stopka"
+        "Rzut kostką 🎲",
+        f"Wyrzuciłeś: **{wynik}**",
+        wybrany_kolor,
+        f"Wywołane przez {ctx.author.name}"
     )
+
     await ctx.send(embed=embed)
+
+
+@bot.command()
+async def awatar(ctx):
+    embed = create_embed(
+        f"Awatar użytkownika {ctx.author.name}",
+        "Oto Twój aktualny awatar:",
+        discord.Color.gold(),
+        "System wizualizacji"
+    )
+
+    embed.set_image(url=ctx.author.display_avatar.url)
+    await ctx.send(embed=embed)
+
 
 if TOKEN is None:
     print("Brak tokena w pliku .env")
